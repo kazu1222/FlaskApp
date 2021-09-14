59a2ca345194c73c7d0eaad68fe0d0706e331391
kazu1222 <12.22.96.kazu@gmail.com>
2021-09-14 12:46:23 +0900

    first commit

---
+++ flaskr/main.py
@@ -0,0 +1,18 @@
+from flaskr import app
+from flask import render_template
+
+
+@app.route('/')
+def index():
+    books = [
+        {'title' : 'はらぺこあおむし',
+        'price' : 1200,
+        'arrival_day' : '2020年7月12日'},
+        {'title' : 'ぐりとぐら',
+        'price' : 990,
+        'arrival_day' : '2020年7月13日'},
+    ]
+    return render_template(
+        'index.html',
+        books = books
+    )
